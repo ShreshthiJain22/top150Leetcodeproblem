526e050f841ecd2f80d2be6ad055f58269b67a64
Shreshthi Badjatya <107920253+ShreshthiJain22@users.noreply.github.com>
2023-12-19 20:36:52 +0530

    Time: 47 ms (34.77%) | Memory: 21.9 MB (92.62%) - LeetSync

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -1,37 +1,85 @@
 class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-      
+        // Save the dimensions of the image.
         int m = img.size();
         int n = img[0].size();
 
-        
-        vector<vector<int>> smoothImg(m, vector<int>(n));
+        // Create temp array of size n.
+        vector<int> temp(n);
+        int prevVal = 0;
 
-      
+        // Iterate over the cells of the image.
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-               
+                // Initialize the sum and count 
                 int sum = 0;
                 int count = 0;
 
-                
-                for (int x = i - 1; x <= i + 1; x++) {
-                    for (int y = j - 1; y <= j + 1; y++) {
-                       
-                        if (0 <= x && x < m && 0 <= y && y < n) {
-                            sum += img[x][y];
-                            count += 1;
-                        }
+                // Bottom neighbors
+                if (i + 1 < m) {
+                    if (j - 1 >= 0) {
+                        sum += img[i + 1][j - 1];
+                        count += 1;
+                    }
+                    sum += img[i + 1][j];
+                    count += 1;
+                    if (j + 1 < n) {
+                        sum += img[i + 1][j + 1];
+                        count += 1;
                     }
                 }
 
-             
-                smoothImg[i][j] = sum / count;
+                // Next neighbor
+                if (j + 1 < n) {
+                    sum += img[i][j + 1];
+                    count += 1;
+                }
+                
+                // This cell
+                sum += img[i][j];
+                count += 1;
+
+                // Previous neighbor
+                if (j - 1 >= 0) {
+                    sum += temp[j - 1];
+                    count += 1;
+                }
+
+                // Top neighbors
+                if (i - 1 >= 0) {
+                    // Left-top corner-sharing neighbor.
+                    if (j - 1 >=  0) {
+                        sum += prevVal;
+                        count += 1;
+                    }
+                    
+                    // Top edge-sharing neighbor.
+                    sum += temp[j];
+                    count += 1;
+
+                    // Right-top corner-sharing neighbor.
+                    if (j + 1 < n) {
+                        sum += temp[j + 1];
+                        count += 1;
+                    }
+                }
+
+                // Store the original value of temp[j], which represents
+                // original value of img[i - 1][j].
+                if (i - 1 >= 0) {
+                    prevVal = temp[j];
+                }
+
+                // Save current value of img[i][j] in temp[j].
+                temp[j] = img[i][j];
+
+                // Overwrite with smoothed value.
+                img[i][j] = sum / count;
             }
         }
 
-       
-        return smoothImg;
+        // Return the smooth image.
+        return img;
     }
 };
