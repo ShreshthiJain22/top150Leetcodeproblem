37aa615108341fbe903e72f3f7a62404412cac19
Shreshthi Badjatya <107920253+ShreshthiJain22@users.noreply.github.com>
2024-01-02 12:41:36 +0530

    Time: 22 ms (10.81%) | Memory: 29.9 MB (83.60%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,25 +1,44 @@
+// class Solution {
+// public:
+//     vector<vector<int>> findMatrix(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         unordered_map<int, int> mp;
+//         for(auto i: nums){
+//             mp[i]++;
+//         }  
+
+//         while(!mp.empty()){
+//             vector<int> temp;
+//             for(auto it = mp.begin(); it != mp.end(); ){
+//                 temp.push_back(it->first);
+//                 it->second--;
+//                 if(it->second == 0){
+//                     it = mp.erase(it);
+//                 } else {
+//                     ++it;
+//                 }
+//             }
+//             ans.push_back(temp);
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq(nums.size() + 1);
+        
         vector<vector<int>> ans;
-        unordered_map<int, int> mp;
-        for(auto i: nums){
-            mp[i]++;
-        }  
-
-        while(!mp.empty()){
-            vector<int> temp;
-            for(auto it = mp.begin(); it != mp.end(); ){
-                temp.push_back(it->first);
-                it->second--;
-                if(it->second == 0){
-                    it = mp.erase(it);
-                } else {
-                    ++it;
-                }
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.push_back({});
             }
-            ans.push_back(temp);
+            
+            // Store the integer in the list corresponding to its current frequency.
+            ans[freq[c]].push_back(c);
+            freq[c]++;
         }
+        
         return ans;
     }
 };
